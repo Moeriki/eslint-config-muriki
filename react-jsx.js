acbbec9f28e16be196f7f76e35d9080f3f31725b
Dieter Luypaert <dieterluypaert@gmail.com>
2016-03-12 11:51:15 +0100

    updated react plugin rules to latest

--- react-jsx.js
+++ react-jsx.js
@@ -14,10 +14,32 @@ module.exports = {
 
   rules: {
 
+    'react/jsx-boolean-value': [1, 'never'],
+    'react/jsx-closing-bracket-location': [1, 'after-props'],
+    'react/jsx-curly-spacing': [1,
+      'never',
+      { allowMultiline: true },
+    ],
+    'react/jsx-equals-spacing': [1, 'never'],
+    'react/jsx-handler-names': [1, {
+      eventHandlerPrefix: 'handle',
+      eventHandlerPropPrefix: 'on',
+    }],
+    'react/jsx-indent-props': [1, 2],
+    'react/jsx-indent': [1, 2],
+    'react/jsx-key': 1,
+    'react/jsx-max-props-per-line': [1, {
+      maximum: 3,
+    }],
+    'react/jsx-no-bind': 2,
     'react/jsx-no-duplicate-props': 2,
+    'react/jsx-no-literals': 1,
     'react/jsx-no-undef': 2,
-    'react/jsx-uses-react': 2,
-    'react/jsx-uses-vars': 2,
+    'react/jsx-pascal-case': 1,
+    'react/jsx-sort-props': 0,
+    'react/jsx-space-before-closing': 1,
+    'react/jsx-uses-react': 1,
+    'react/jsx-uses-vars': 1,
 
   },
 
--- react.js
+++ react.js
@@ -11,17 +11,40 @@ module.exports = {
     /**
      * eslint-plugin-react
      */
-    'react/display-name': [1, { acceptTranspilerName: true }],
-    'react/no-danger': 2,
+
+    'react/display-name': [1, { ignoreTranspilerName: false }],
+    'react/forbid-prop-types': 0,
+    'react/no-danger': 1,
     'react/no-deprecated': 2,
     'react/no-did-mount-set-state': 2,
     'react/no-did-update-set-state': 2,
     'react/no-direct-mutation-state': 2,
     'react/no-is-mounted': 2,
+    'react/no-multi-comp': 2,
+    'react/no-set-state': 0,
+    'react/no-string-refs': 2,
     'react/no-unknown-property': 2,
+    'react/prefer-es6-class': 0,
+    'react/prefer-stateless-function': 1,
     'react/prop-types': 1,
     'react/react-in-jsx-scope': 2,
+    'react/require-extension': 0,
+    'react/self-closing-comp': 1,
+    'react/sort-comp': 1,
+    'react/sort-prop-types': [1, {
+      callbacksLast: true,
+      ignoreCase: true,
+      requiredFirst: true,
+    }],
+    'react/wrap-multilines': 1,
 
+  },
+
+  settings: {
+    react: {
+      pragma: 'React',
+      version: '0.14.0',
+    },
   },
 
 }
