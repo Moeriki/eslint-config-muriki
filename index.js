92c853bffc9811eecb0d75b48af0eb731c25be21
Dieter Luypaert <dieterluypaert@gmail.com>
2016-07-07 10:22:46 +0200

    revert path work-around for eslint bug

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 'use strict'
 
-var path = require('path')
-
 module.exports = {
-
   extends: [
-    path.resolve(__dirname, './base.js'),
-    path.resolve(__dirname, './style.js'),
+    './base.js',
+    './style.js',
   ],
-
 }
