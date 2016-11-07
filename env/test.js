43b8423a95a6d8c7d9e4a0b0619eb5b69a65994c
Dieter Luypaert <dieterluypaert@gmail.com>
2016-11-07 16:59:32 +0100

    allow many more statements in tests

--- env/test.js
+++ env/test.js
@@ -15,6 +15,7 @@ module.exports = {
     'arrow-body-style': 0,
     'func-names': 0,
     'handle-callback-err': 0,
+    'max-statements': [1, 32],
     'no-unused-expressions': 0,
     'no-sync': 0,
 
