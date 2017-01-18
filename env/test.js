684324ed22c458efa0786fe5bfa112eb8d5d246d
Dieter Luypaert <dieterluypaert@gmail.com>
2017-01-18 21:31:40 +0100

    loosen test rules max-lines and no-magic-numbers

--- env/test.js
+++ env/test.js
@@ -15,7 +15,9 @@ module.exports = {
     'arrow-body-style': 0,
     'func-names': 0,
     'handle-callback-err': 0,
+    'max-lines': 0,
     'max-statements': [1, 32],
+    'no-magic-numbers': 0,
     'no-unused-expressions': 0,
     'no-sync': 0,
 
