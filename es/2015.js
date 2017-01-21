b1dda94b23a1820a1b56299fddb07f835aa52710
Dieter Luypaert <dieterluypaert@gmail.com>
2017-01-21 14:25:21 +0100

    move non-blocking ES2015 rules to index

--- es/2015.js
+++ es/2015.js
@@ -15,53 +15,16 @@ module.exports = {
   rules: {
 
     /**
-     * Potential Errors
-     */
-
-    'constructor-super': 2,
-    'no-class-assign': 2,
-    'no-const-assign': 2,
-    'no-dupe-class-members': 2,
-    'no-new-symbol': 2,
-    'no-this-before-super': 2,
-    'require-yield': 2,
-
-    /**
      * Best Practices
      */
 
-    'class-methods-use-this': 1,
     'no-param-reassign': 1,
-    'no-confusing-arrow': 0,
-    'no-useless-constructor': 1,
-    'no-useless-rename': 1,
-    'no-template-curly-in-string': 1,
     'object-shorthand': 1,
     'prefer-const': [1, { ignoreReadBeforeAssign: true }],
     'prefer-destructuring': 1,
-    'prefer-numeric-literals': 1,
-    'prefer-promise-reject-errors': 2,
     'prefer-rest-params': 1,
     'prefer-spread': 1,
     'prefer-template': 1,
-    'symbol-description': 1,
-
-    /**
-     * Stylistic Consistency
-     */
-
-    'arrow-body-style': 1,
-    'arrow-parens': 1,
-    'arrow-spacing': 1,
-    'generator-star-spacing': [1, {
-      before: false,
-      after: true,
-    }],
-    'no-useless-computed-key': 1,
-    'rest-spread-spacing': [1,
-      'never',
-    ],
-    'yield-star-spacing': 1,
 
   },
 
